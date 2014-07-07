04fa7bfb1dec5e8f4ebdd77f33d97ea5ab5d8c23
Francois Daoust <fd@tidoust.net>
2014-07-07 17:18:18 +0200

    Chromecast devices supported through Google Cast Extension
    The demo may now send the video to Chromecast devices, provided the page runs
    in Chrome with the Google Cast Extension installed:
    https://chrome.google.com/webstore/detail/google-cast/boadgeojelhgndaghljhdicfkmllpafd?hl=en
    
    The page loads and initializes the Google Cast API on startup.
    
    When the user hits the "share" button, the page:
    1. tries to create a session on a Chromecast device if the API reports there
    are such devices available. Chrome will typically prompt the user to select
    a Chromecast device in the list of devices available.
    2. falls back to previous behavior if there are no Chromecast devices around
    or if the user declines to use a Chromecast device.
    
    Notes:
    - if the user declines to use a Chromecast device, the call to
    "window.open" occurs in a fallback function and Chrome will block it by
    default. Pop-ups need to be allowed for the fallback to work in that case.
    - the video only pauses on the initial page when it is ready to play on the
    Chromecast device. From a UX perspective, it might be better to pause the
    video right away though.
    - sending the video to a nearby Chromecast device may take a few seconds,
    the code does not implement any sort of protection mechanism: it must be
    relatively easy to break the demo if you click around before an action is
    over.
    - the video content-type is hardcoded for the time being. Chromecast needs it
    to play the video, it seems. The content-type could be read from the video
    element instead.
    - the page keeps the Chromecast session open even if the user stops sharing
    the video. The session can be terminated through the extension menu. The
    page could perhaps close the session permanently, not sure which approach is
    better.

--- requestshowmedia.js
+++ requestshowmedia.js
@@ -8,13 +8,18 @@ var video = null,
     pause = HTMLMediaElement.prototype.pause;
     // var fastSeek = HTMLMediaElement.prototype.fastSeek; // not implemented by browsers yet
 
+var castAvailable = false,  // Set when at least one Cast device is available
+    castSession = null;     // Cast session linked to the page if there's one
+
 HTMLVideoElement.prototype.requestShow = function () {
   video = this;
   requestShowMedia(this);
 };
 
 HTMLVideoElement.prototype.exitShow = function () {
-  video.player.close();
+  if (this.dataset.secondScreenEnabled === 'true') {
+    this.player.close();
+  }
 };
 
 HTMLMediaElement.prototype.play = function (args) {
@@ -28,7 +33,7 @@ HTMLMediaElement.prototype.play = function (args) {
 HTMLMediaElement.prototype.pause = function (args) {
   if (this.dataset.secondScreenEnabled  === 'true') {
     video.player.postMessage({ cmd: 'pause'}, playerOrigin);
-  } {
+  } else {
     pause.apply(this, args);
   }
 };
@@ -42,14 +47,73 @@ HTMLMediaElement.prototype.fastSeek = function (currentTime) {
   }
 };
 
+function onSecondScreenEnabled() {
+  video.pause();
+  video.style.opacity = 0.5;
+  video.dataset.secondScreenEnabled = true;
+}
+
+function onSecondScreenDisabled(currentTime) {
+  video.dataset.secondScreenEnabled = false;
+  video.player = null;
+  video.style.opacity = 1;
+  if (currentTime || (currentTime === 0)) {
+    video.currentTime = currentTime;
+  }
+  video.play();
+}
+
+function dispatchTimeUpdateOnSecondScreenEvent(currentTime) {
+  try {
+    video.ontimeupdateonsecondscreen(currentTime);
+  } catch (ex) { }
+
+  video.dispatchEvent(new CustomEvent('timeupdateonsecondscreen',
+    { detail: { currentTimeOnSecondScreen: currentTime } }));
+}
+
+function dispatchSecondScreenCloseEvent() {
+  try {
+    video.onclosesecondscreen();
+  } catch (ex) { }
+  video.dispatchEvent(new Event('closesecondscreen'));
+}
+
 function requestShowMedia(video) {
+  // Try projecting to Chromecast device first
+  if (castAvailable) {
+    console.log('Cast device available');
+    if (castSession) {
+      loadCastMedia(castSession);
+    }
+    else {
+      chrome.cast.requestSession(loadCastMedia, function (error) {
+        if (error.code === 'cancel') {
+          console.info('User chose not to use Cast device, fallback to attached screen');
+          requestShowMediaOnAttachedScreen(video);
+        }
+        else {
+          console.error('Could not create Cast session', error);
+          dispatchSecondScreenCloseEvent();
+        }
+      });
+    }
+  }
+  else {
+    requestShowMediaOnAttachedScreen(video);
+  }
+}
+
+function requestShowMediaOnAttachedScreen(video) {
   var playerURL = document.querySelector('script[src$="requestshowmedia.js"]').dataset.player;
   video.player = window.open(playerURL, '', 'presentation');
-  video.dataset.secondScreenEnabled = true;
+  if (!video.player) {
+    dispatchSecondScreenCloseEvent();
+    return;
+  }
   
   video.player.addEventListener('load', function () {
-    video.pause();
-    video.style.opacity = 0.5;
+    onSecondScreenEnabled();
     video.player.postMessage({
       cmd: 'open',
       currentSrc: video.currentSrc,
@@ -60,25 +124,13 @@ function requestShowMedia(video) {
   addEventListener('message', function (event) {
     switch (event.data.cmd) {
       case 'play':
-        video.dataset.secondScreenEnabled = false;
-        video.style.opacity = 1;
-        video.currentTime = event.data.currentTime;
-        video.play();
+        onSecondScreenDisabled(event.data.currentTime);
         break;
       case 'ontimeupdateonsecondscreen':
-        try {
-          video.ontimeupdateonsecondscreen(event.data.currentTime);
-        } catch (ex) { }
-
-        video.dispatchEvent(new CustomEvent('timeupdateonsecondscreen',
-          { detail: { currentTimeOnSecondScreen: event.data.currentTime } }));
+        dispatchTimeUpdateOnSecondScreenEvent(event.data.currentTime);
         break;
       case 'close':
-        try {
-          video.onclosesecondscreen();
-        } catch (ex) { }
-
-        video.dispatchEvent(new Event('closesecondscreen'));
+        dispatchSecondScreenCloseEvent();
         break;
       default:
         break;
@@ -86,6 +138,133 @@ function requestShowMedia(video) {
   });
 
   return video.player;
+}
+
+// Initialize the Cast API if available
+window['__onGCastApiAvailable'] = function (loaded, errorInfo) {
+  if (!loaded) {
+    console.error('Cast API could not be loaded', errorInfo);
+    return;
+  }
+
+  var applicationID = chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID;
+  var sessionRequest = new chrome.cast.SessionRequest(applicationID);
+  var apiConfig = new chrome.cast.ApiConfig(
+    sessionRequest,
+    function (session) {
+      console.log('Cast session found at startup');
+      castSession = session;
+      castSession.addUpdateListener(onCastSessionChange);
+    },
+    function (availability) {
+      console.log('Cast API', 'availability=' + availability);
+      if (availability === chrome.cast.ReceiverAvailability.AVAILABLE) {
+        castAvailable = true;
+      }
+      else {
+        // No more Chromecast device around, stop casting session
+        // if there was one.
+        castAvailable = false;
+        if (castSession) {
+          onSecondScreenDisabled();
+          dispatchSecondScreenCloseEvent();
+        }
+      }
+    });
+  chrome.cast.initialize(apiConfig, function () {
+    console.info('The Cast API was initialized');
+  }, function (error) {
+    console.error('The Cast API could not be initialized', error);
+  });
+};
+
+// TODO: Video content-type should not be hardcoded. It cannot simply be dropped
+// as Cast devices refuse to play the video otherwise.
+function loadCastMedia(session) {
+  var mediaInfo = new chrome.cast.media.MediaInfo(video.currentSrc, 'video/mp4');
+  var request = new chrome.cast.media.LoadRequest(mediaInfo);
+  request.currentTime = video.currentTime;
+  castSession = session;
+  castSession.addUpdateListener(onCastSessionChange);
+  castSession.loadMedia(request, onCastMediaLoaded, onCastMediaError);
+}
+
+function onCastSessionChange(isAlive) {
+  if (!isAlive) {
+    castSession = null;
+    if (video && (video.dataset.secondScreenEnabled === 'true')) {
+      onSecondScreenDisabled();
+      dispatchSecondScreenCloseEvent();
+    }
+  }
+}
+
+function onCastMediaLoaded(media) {
+  video.player = new CastPlayer(media);
+  onSecondScreenEnabled();
+}
+
+function onCastMediaError(error) {
+  console.error('The Cast API returned an error', error);
+  onSecondScreenDisabled();
+  dispatchSecondScreenCloseEvent();
+}
+
+function CastPlayer(media) {
+  this.postMessage = function (msg) {
+    switch (msg.cmd) {
+    case 'play':
+      // Note: calling "play" when a Chromecast device is done playing a video
+      // does not do anything. The only way to restart play is to reload the
+      // video, it seems.
+      if ((media.playerState === chrome.cast.media.PlayerState.IDLE) &&
+          (media.idleReason === chrome.cast.media.IdleReason.FINISHED)) {
+        video.currentTime = 0;
+        loadCastMedia(castSession);
+      }
+      else {
+        media.play();
+      }
+      break;
+    case 'pause':
+      media.pause();
+      break;
+    case 'fastSeek':
+      seekRequest = new chrome.cast.media.SeekRequest();
+      seekRequest.currentTime = msg.currentTime;
+      media.seek(seekRequest);
+      break;
+    }
+  };
+
+  this.close = function () {
+    var currentTime = media.getEstimatedTime();
+    if (castInterval) {
+      clearInterval(castInterval);
+      castInterval = null;
+    }
+    media.stop(null, function () {
+      onSecondScreenDisabled(currentTime);
+      dispatchSecondScreenCloseEvent();
+    }, function (error) {
+      console.warn('Could not properly stop Cast session', error);
+      onSecondScreenDisabled();
+      dispatchSecondScreenCloseEvent();
+    });
+  };
+
+  // Monitor time updates
+  var castInterval = setInterval(function () {
+    var currentTime = null;
+    if (castSession) {
+      currentTime = media.getEstimatedTime();
+      dispatchTimeUpdateOnSecondScreenEvent(currentTime);
+    }
+    else {
+      clearInterval(castInterval);
+      castInterval = null;
+    }
+  }, 500);
 }
 
 }());
